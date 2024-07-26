c35a5a7fa9217ea910caac4287a08f2335f5515b
Paulo <paulonog061@gmail.com>
2024-07-25 22:02:57 -0300

    first

---
+++ app/src/main/java/br/com/paulo/projeto_hp12c/Calculadora.java
@@ -0,0 +1,13 @@
+package br.com.paulo.projeto_hp12c;
+
+import java.util.Deque;
+
+public class Calculadora {
+    public static final int MODO_EXIBINDO = 0;
+    public static final int MODO_EDITANDO = 1;
+    
+    private double setNumero;
+    private double getNumero;
+    private Deque<>;
+
+}
